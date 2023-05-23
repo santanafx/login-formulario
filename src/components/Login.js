e68ae958a3b039d9468ddf440a2197c6386a6e45
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-23 11:13:34 -0300

    Atualização de código Login.js
    Foi adicionado validação de usuário e senha

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,41 @@
 import React from 'react'
 import './Login.css'
+import { Context } from '../context/globalContext';
 
 export const Login = () => {
+
+    const { dataBase } = React.useContext(Context);
+    const [usuario, setUsuario] = React.useState('');
+    const [usuarioInvalidado, setUsuarioInvalidado] = React.useState('');
+    const [senha, setSenha] = React.useState('');
+    const [senhaInvalidada, setSenhaInvalidada] = React.useState('');
+
+    const fazerLogin = () => {
+        dataBase.forEach(element => {
+            if (element.usuario === usuario && element.senha === senha) {
+                setUsuarioInvalidado(false)
+                setSenhaInvalidada(false)
+            } else {
+                setUsuarioInvalidado(true)
+                setSenhaInvalidada(true)
+            }
+        });
+    }
+
     return (
         <section className='loginContainerBg'>
             <div className='loginContainer'>
                 <h1>Faça o seu login:</h1>
                 <div>
                     <label htmlFor='usuario'>Usuário</label>
-                    <input className='loginInput' type="text" id='usuario' placeholder='Digite o usuário.' />
+                    <input className='loginInput' type="email" id='usuario' placeholder='Digite o usuário.' value={usuario} onChange={(event) => setUsuario(event.target.value)} />
+                    {usuarioInvalidado ? <span style={{ color: 'red' }}>Usuário ou senha inválido.</span> : ''}
                     <label htmlFor='senha'>Senha</label>
-                    <input className='loginInput' type="password" id='senha' placeholder='Digite sua senha.' />
+                    <input className='loginInput' type="password" id='senha' placeholder='Digite sua senha.' value={senha} onChange={(event) => setSenha(event.target.value)} />
+                    {senhaInvalidada ? <span style={{ color: 'red' }}>Usuário ou senha inválido.</span> : ''}
                 </div>
                 <div className='loginButton'>
-                    <button>Entrar</button>
+                    <button onClick={() => fazerLogin()}>Entrar</button>
                 </div>
             </div>
         </section>
